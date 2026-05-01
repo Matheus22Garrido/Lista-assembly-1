120cea9be55e8e814b826cf3f71fbb0187786f05
Matheus22Garrido <theusgarrido22@gmail.com>
2026-05-01 23:23:36 +0000

    ex 9 e 10 ok

--- Lista1/Ex10.asm
+++ Lista1/Ex10.asm
@@ -1,9 +1,90 @@
 .data
-    msg1: .asciiz "\n"
-    msg2: .asciiz "\n"
-    msg3: .asciiz "\n"
-    msg4: .asciiz "\n"
-    msg5: .asciiz "\n"
-    msg6: .asciiz "\n"
+    msg1: .asciiz "\nDigite o primeiro número: "
+    msg2: .asciiz "\nDigite o segundo número: "
+    msg3: .asciiz "\nO resto da divisão do maior pelo menor é: "
+    msg4: .asciiz "\nOs números são iguais, o resto é sempre 0"
+    msg5: .asciiz "\nNão pode dividir por 0"
 .text
 main:
+    # recebe n1
+    li $v0, 4
+    la $a0, msg1
+    syscall
+    li $v0, 5
+    syscall
+    add $t9, $v0, 0
+
+    # recebe n2
+    li  $v0, 4
+    la $a0, msg2
+    syscall
+    li $v0, 5
+    syscall
+    add $t8, $v0, 0
+
+    # decisão
+    bgt $t9, $t8, maior
+    blt $t9, $t8, menor
+    beq $t9, $t8, iguais
+
+maior:
+    # $t9 ($s0) = maior, $t8 ($s1) = menor
+    add $s0, $t9, 0
+    add $s1, $t8, 0
+    beq $s0, 0, zero
+    beq $s1, 0, zero
+    j aritmetica
+
+menor:
+    # $t9 ($s1) = menor, $t8 ($s0) = maior
+    add $s0, $t8, 0
+    add $s1, $t9, 0
+    beq $s0, 0, zero
+    beq $s1, 0, zero
+    j aritmetica
+
+iguais:
+    li $v0, 4
+    la $a0, msg4
+    syscall
+    j fim
+
+aritmetica:
+    # faz as contas  //  $s7 = resto da divisao  //  $t4 = contador
+    add $t4, $t4, 1
+
+if:
+    beq $t4, 1, parte1
+    j parte2
+
+parte1:
+    # quando contador é 1
+    sub $s7, $s0, $s1
+    j verifica
+
+parte2:
+    # quando contador n é mais 1
+    sub $s7, $s7, $s1
+    j verifica
+
+verifica:
+    # verifica
+    bge $s7, $s1, aritmetica
+    j resultado
+
+resultado:
+    li $v0, 4
+    la $a0, msg3
+    syscall
+    li $v0, 1
+    add $a0, $s7, 0
+    syscall
+    j fim
+
+zero:
+    li $v0, 4
+    la $a0, msg5
+    syscall
+    j main
+fim:
+
--- Lista1/Ex9.asm
+++ Lista1/Ex9.asm
@@ -1,9 +1,45 @@
 .data
-    msg1: .asciiz "\n"
-    msg2: .asciiz "\n"
-    msg3: .asciiz "\n"
-    msg4: .asciiz "\n"
-    msg5: .asciiz "\n"
-    msg6: .asciiz "\n"
+    msg1: .asciiz "\nDigite seu primeiro número: "
+    msg2: .asciiz "\nDigite seu segundo número (deve ser diferente de zero): "
+    msg3: .asciiz "\nEu disse >DIFERENTE DE ZERO<"
+    msg4: .asciiz "\nO resultado da divisão é: "
 .text
 main:
+    # recebe n1
+    li $v0, 4
+    la $a0, msg1
+    syscall
+    li $v0, 5
+    syscall 
+    add $t9, $v0, 0
+
+n2:
+    # recebe n2
+    li $v0, 4
+    la  $a0, msg2
+    syscall
+    li $v0, 5
+    syscall
+    add $t8, $v0, 0
+
+    # ve se é zero
+    beq $t8, 0, seZero
+    j aritmetica
+
+seZero:
+    # fala que nao pode ser zero
+    li $v0, 4
+    la $a0, msg3
+    syscall
+    j n2
+
+aritmetica:
+    # aritmetica
+    div $s0, $t9, $t8
+
+    li $v0, 4
+    la $a0, msg4
+    syscall
+    li $v0, 1
+    add $a0, $s0, 0
+    syscall
